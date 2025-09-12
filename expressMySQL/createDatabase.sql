167d8ea990974d55cccf6de3bd56295720ed91bf
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2025-09-12 12:03:15 -0300

    Rota inserir no bd

---
+++ expressMySQL/createDatabase.sql
@@ -0,0 +1,15 @@
+-- Criação do banco de dados
+CREATE DATABASE express_ti38;
+
+-- Seleciona o banco de dados para uso
+USE express_ti38;
+
+-- Criação da tabela alunos
+CREATE TABLE alunos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    nota1 DECIMAL(5,2),
+    nota2 DECIMAL(5,2),
+    nota3 DECIMAL(5,2),
+    nota4 DECIMAL(5,2)
+);
--- expressMySQL/index.js
+++ expressMySQL/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const con = require('./controller/bd')()
+let con = require('./controller/bd')()
 const statusCon = con?'Conectado ao banco!':''
 let app = express()
 const PORTA = '3201'
@@ -12,6 +12,21 @@ app.use(express.json())
 app.get('/', (req, res)=>res.sendFile(`${BASEDIR}/index.html`))
 app.get('/cadastrar', (req, res)=>res.sendFile(`${BASEDIR}/cadastrar.html`))
 
+//Inserir registro Aluno
+app.post('/cadastrar', (req, res)=>{
+    let dados = req.body
+    dados = [dados.nome, parseFloat(dados.nota1), parseFloat(dados.nota2), parseFloat(dados.nota3), parseFloat(dados.nota4)]
+    let sql = `
+                INSERT INTO alunos (nome, nota1, nota2, nota3, nota4)
+                VALUES (?, ?, ?, ?, ?);
+    `
+    con.query(sql, dados, (erro, resp)=>{
+        let resposta
+        if(erro) resposta = {...erro, status:400, message: `Os dados não foram gravados`}
+        else resposta = {...resp, status:201}
+        res.json(resposta)
+    })
+})
 
 
 app.use((req, res)=>res.sendFile(`${BASEDIR}/404.html`))
