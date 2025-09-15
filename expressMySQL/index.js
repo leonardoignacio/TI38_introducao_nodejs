226907e29e3fa8b670502b7eb245eae3b39fcfec
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2025-09-15 11:51:27 -0300

    Rotas de atulaização e exclusão do CRUD

--- expressMySQL/index.js
+++ expressMySQL/index.js
@@ -40,6 +40,35 @@ app.get('/alunos', (req, res)=>{
     })
 })
 
+// U - Atualização
+app.patch('/alunos/:id', (req, res)=>{
+    let dados = req.body
+    dados = [dados.nome, parseFloat(dados.nota1), parseFloat(dados.nota2), parseFloat(dados.nota3), parseFloat(dados.nota4)]
+    let id = parseInt(req.params.id)
+    let sql = `UPDATE express_ti38.alunos 
+    SET nome=?, nota1=?, nota2=?, nota3=?, nota4=? 
+    WHERE id=${id};`
+    con.query(sql, dados, (err, resp)=>{
+        let resposta
+        if (err) resposta = {...err, status:400, message:'Falha na atualização!'}
+        else resposta = {...resp, status:200, message:`Registro atualizado com sucesso!`}
+        res.json(resposta)
+    })
+})
+
+// D - Excluir registros
+app.delete('/alunos/:id', (req, res)=>{
+    let id = parseInt(req.params.id)
+    let sql = `DELETE FROM alunos WHERE id=${id};`
+    con.query(sql,(err, resp)=>{
+        let resposta
+        if (err) respota = {...err, status:400, message:'Erro na exclusão de dados.'}
+        else resposta = {...resp, status:200, message:'Registro exclído com sucesso!'}
+        res.json(resposta)
+    })
+})
+
+
 app.use((req, res)=>res.sendFile(`${BASEDIR}/404.html`))
 
 app.listen(PORTA,()=>console.log(`Rodando em: http://localhost:${PORTA} \n${statusCon}`))
